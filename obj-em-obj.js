c341be604ded3df217acf4ee0995700038adf0d6
Gabrielle <gabicaram@icloud.com>
2024-02-22 01:09:18 -0300

    fazendo uma lista de obj

--- obj-em-obj.js
+++ obj-em-obj.js
@@ -3,14 +3,26 @@ const aluno = {
     idade: 24,
     email: "joao@firma.com",
     telefone: ["1155555550", "1144444440"],
+    bolsista: true,
   };
   
-  aluno.endereco = {
+  aluno.endereco = [{
     rua: "R. Joseph Climber",
     numero: 1337,
     apartamento: true,
     complemento: "ap 934",
-  };
+  }];
+
+  aluno.endereco.push({
+    rua: 'Rua Dona Clotilde',
+    numero: '71',
+    complemento: ''
+  })
 
   console.log(aluno)
-  console.log(aluno["endereco"]);
+  console.log(aluno["endereco"][0].rua); // acesso por colchetes
+  console.log(aluno.endereco[1].rua) // acesso por ponto
+
+  const listaApartamentos = aluno.endereco.filter((endereco) => endereco.apartamento)
+
+  console.log(listaApartamentos)
