3b7bac41d42c73f00f5fb9092391dd6e3062ce07
Gabrielle <gabicaram@icloud.com>
2024-02-22 23:05:48 -0300

    desafio 1 utilizando json

---
+++ desafio1-utilizar-require.js
@@ -0,0 +1,10 @@
+// 1 - Crie um arquivo chamado dados.json contendo informações fictícias em formato JSON. 
+// O arquivo pode representar, por exemplo, dados de produtos, usuários ou qualquer outra informação que você deseje.
+
+// Crie um arquivo chamadoutilizarRequire.js e, dentro dele, utilize a função require() para importar o conteúdo do arquivo dados.json.
+// Imprima no console o conteúdo importado utilizando a função console.log().
+// Acesse diferentes propriedades do objeto importado e imprima no console para verificar o acesso aos dados.
+
+const dados = require('./dados');
+console.log(dados)
+
