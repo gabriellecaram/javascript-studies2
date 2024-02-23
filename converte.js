849ef49f232959b84cb159e5e131e6778bbb2cbd
Gabrielle <gabicaram@icloud.com>
2024-02-23 03:01:46 -0300

    conversão obj->string(json)->obj

--- converte.js
+++ converte.js
@@ -3,10 +3,9 @@ console.log("Importa de JSON para objeto:")
 console.log(typeof estudante, '\n', estudante, )
 
 // quando precisa exportar um JSON precisa converter tudo para string
-const stringEstudante = JSON.stringify(estudante);
-
 console.log()
 console.log("Transforma de Objeto para String:")
+const stringEstudante = JSON.stringify(estudante);
 console.log(stringEstudante);
 console.log(typeof stringEstudante);
 
@@ -14,10 +13,8 @@ console.log(stringEstudante.nome) // undefined
 // não existem mais propriedades, nao tem como acessar propriedades
 
 // para re-converter para objeto:
-
-const objEstudante = JSON.parse(stringEstudante);
-
 console.log()
 console.log("Transforma de String para Objeto")
+const objEstudante = JSON.parse(stringEstudante);
 console.log(typeof objEstudante)
 console.log(objEstudante)
